b16876a876a0bf768850631c39c7bd2eb3734521
toBeOfUse <mitchjacov@gmail.com>
2022-08-29 17:34:27 -0400

    remove old unneeded HelloWorld field

--- lib/pages/helloworld.dart
+++ lib/pages/helloworld.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class HelloWorld extends StatelessWidget {
   const HelloWorld({Key? key}) : super(key: key);
-  static const name = "Hello World";
 
   @override
   Widget build(BuildContext context) {
